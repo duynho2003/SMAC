f001bf718f4f40cafc8bfe47613e3d2bc8c73f14
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-16 18:07:41 +0700

    up

--- Lab02/Controllers/EmployeeController.cs
+++ Lab02/Controllers/EmployeeController.cs
@@ -10,10 +10,20 @@ namespace Lab02.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public IActionResult Index(string fname)
         {
-            return View(_db.Employees.ToList());
+            var model = _db.Employees.ToList();
+            if (string.IsNullOrEmpty(fname))
+            {
+                return View(model);
+            }
+            else
+            {
+                model=model.Where(e => e.FirstName!.Contains(fname)).ToList();
+                return View(model);
+            }
         }
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -34,6 +44,56 @@ namespace Lab02.Controllers
                     _db.Employees.Add(employees);
                     _db.SaveChanges();
                     return RedirectToAction("Index");
+                }
+                else 
+                {
+                    ModelState.AddModelError(string.Empty, "Fail...");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EditOrDelete(int employeeid)
+        {
+            var employee=_db.Employees.Find(employeeid);
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult EditOrDelete(string submits, Employees employees, IFormFile file)
+        {
+            try
+            {
+                var model = _db.Employees.Find(employees.EmployeeID);
+                if (model!=null && ModelState.IsValid)
+                {
+                    if (submits.Equals("Update"))
+                    {
+                        if(file!=null)
+                        {
+                            string path = Path.Combine("wwwroot/images", file.FileName);
+                            var stream = new FileStream(path, FileMode.Create);
+                            file.CopyTo(stream);
+                        }
+                        employees.Photo = file != null ? "images/" + file.FileName : model.Photo;
+
+                        model.LastName = employees.LastName;
+                        model.FirstName = employees.FirstName;
+                        model.BirthDate = employees.BirthDate;
+                        model.Photo = employees.Photo;
+                        model.Skills = employees.Skills;
+                        _db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        _db.Employees.Remove(model); 
+                        _db.SaveChanges(); 
+                        return RedirectToAction("Index");
+                    }
                 }
                 else 
                 {
